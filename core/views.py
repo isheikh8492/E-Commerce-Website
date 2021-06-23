aedcafd9da1e8b69b96ccc73afcc14fd7d363c8f
Imaduddin Sheikh <isheikh8492@gmail.com>
2021-06-23 11:36:35 -0500

    home template name change

--- core/views.py
+++ core/views.py
@@ -5,11 +5,11 @@ from django.views.generic import ListView, View
 # Create your views here.
 
 
-def item_list(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, "home-page.html", context)
+# def item_list(request):
+#     context = {
+#         'items': Item.objects.all()
+#     }
+#     return render(request, "home-page.html", context)
 
 
 class HomeView(ListView):
