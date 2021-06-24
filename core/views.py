a59e529d647db19e2fe4b5109b4b23e3c01b6705
Imaduddin Sheikh <isheikh8492@gmail.com>
2021-06-24 17:23:51 -0500

    adding items 2 cart commit 2

--- core/views.py
+++ core/views.py
@@ -1,9 +1,9 @@
 from typing import List
 from django.http import request
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Item
-from django.views.generic import ListView
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Item, OrderItem, Order
+from django.views.generic import ListView, DetailView
+from django.utils import timezone
 
 # Create your views here.
 
@@ -27,3 +27,45 @@ def products(request):
         'items': Item.objects.all()
     }
     return render(request, "product.html", context)
+
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(item=item,
+                                                          user=request.user,
+                                                          ordered=False)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+        else:
+            order.items.add(order_item)
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+    return redirect("core:product", slug=slug)
+
+
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(item=item,
+                                                  user=request.user,
+                                                  ordered=False)[0]
+            order.items.remove(order_item)
+        else:
+            # add a message saying user doesn't have an order
+            return redirect("core:product", slug=slug)
+    else:
+        # add a message saying user doesn't have an order
+        return redirect("core:product", slug=slug)
+    return redirect("core:product", slug=slug)
